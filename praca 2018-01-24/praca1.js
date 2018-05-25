470eb3c835a71afbf2bdd4a7224788226f7ffec9
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-05-25 15:30:00 +0200

    initial commit

---
+++ praca 2018-01-24/praca1.js
@@ -0,0 +1,47 @@
+//zadania zestaw 1:
+// 1
+db.createCollection("animals")
+// 2
+db.animals.insertMany([
+    {"type" : "dog"},
+    {"type" : "cat"},
+    {"type" : "penguin"},
+    {"type" : "fox"},
+    {"type" : "whale"},
+    {"type" : "ostrich"}
+])
+// 3
+db.animals.count()
+// 4
+db.animals.insert({"type" : "monkey"})
+// 5
+db.animals.update(
+    {"type" : "monkey"},
+    {
+        $set: {"type" : "donkey"},
+        $set: {"age" : 3}
+    }
+)
+// 6
+db.createCollection("plants")
+// 7
+db.plants.insertMany([
+    {"type" : "rose"},
+    {"type" : "sunflower"}
+])
+// 8
+db.createCollection("colours")
+// 9
+db.colours.insertMany([
+    {"type" : "red"},
+    {"type" : "yellow"}
+])
+// 10
+db.plants.renameCollection("flowers")
+// 11
+db.animals.remove({"type" : "fox"})
+// 12
+db.flowers.drop()
+// ---------- pomocnicze
+db.animals.find()
+db.colours.find()
---
+++ praca 2018-01-25/rozwiazaniazestaw2.js
@@ -0,0 +1,112 @@
+// rozwiazania zestaw 2:
+// 1
+db.stations.find({"name" : "Kawaihae"}).count()
+// 2
+db.stations.find(
+    {
+        "metadata.location.state" : "HI"
+    }
+)
+// 3
+db.stations.find(
+    {},
+    {
+        "_id" : 0,
+        "-name" : 1,
+        "metadata.location.state" : 1
+    }
+).sort({"_id" : 1})
+// 4
+db.stations.find(
+    {"metadata.date_established" : {$lt : "1960-01-01"}}
+)
+// 5
+db.stations.find(
+    {
+        "parameter" : {
+            $not : {
+                $elemMatch : {
+                    "-name" : "Air Temp"
+                }
+            }
+        }
+    }
+)
+// 6
+db.stations.find(
+    {
+        $and : [
+            {"parameter.-name" : "Air Temp"},
+            {"parameter.-name" : "Water Temp"}
+        ]
+    }
+)
+// 7
+db.stations.find(
+    {
+        "parameter" : {
+            $exists : false
+        }
+    }
+)
+// 8
+db.ocean_data.find(
+    {
+        "products" : {
+            $elemMatch : {
+                "name" : "air_temperature",
+                "v" : {
+                    $not : {
+                        $type : "double"
+                    }
+                }
+            }
+        }
+    }
+)
+// 9
+db.ocean_data.find(
+    {
+        $and : [
+            {
+                "products" : {
+                    $elemMatch : {
+                        "name" : "air_temperature",
+                        "v" : {$lt : 70, $gt : 60}
+                    }
+                }
+            },
+            {
+                "products" : {
+                    $elemMatch : {
+                        "name" : "air_pressure",
+                        "v" : {$gt : 1000}
+                    }
+                }
+            }
+        ]
+    }
+)
+
+db.ocean_data.find()
+
+
+
+/*db.ocean_data.find(
+    {
+        "products" : {
+            $elemMatch : {
+                        "name" : "air_temperature",
+                        "v" : {$lt : 70, $gt : 60}
+            }
+        }
+    }
+)*/
+
+
+
+
+
+
+    
+db.stations.find()
